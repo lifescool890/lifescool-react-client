35cf2caea881ad7a113ceb6a84c64b35f728d82b
amanmarakkar <amanahammedkt@gmail.com>
2023-11-25 15:03:33 +0530

    many changes

--- src/constants/axios.ts
+++ src/constants/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 
-const adminBaseURL = "http://localhost:3000/admin"
-const userBaseURL = "http://localhost:3000/"
+const adminBaseURL = import.meta.env.VITE_ADMIN_BASE_URL
+const userBaseURL = import.meta.env.VITE_USER_BASE_URL
 const adminApi = axios.create({
 	baseURL: adminBaseURL,
 	responseType: "json",
