34c10978922fa337c1a301bab3975848d52b09d7
amanmarakkar <amanahammedkt@gmail.com>
2023-11-29 10:59:37 +0530

    TEXT

--- src/pages/CourseDetails.tsx
+++ src/pages/CourseDetails.tsx
@@ -5,19 +5,21 @@ import CourseBannerTwo from "../componenets/User/courseDetailBannerTwo/index";
 import CourseBannerThree from "../componenets/User/courseDetailBannerThree/index";
 import CourseBannerFour from "../componenets/User/courseDetailBannerFour/index";
 import CourseBannerFive from "../componenets/User/courseDetailBannerFive/index";
-import CourseBannerSix from "../componenets/User/courseDetailBannerSix/index"
+import CourseBannerSix from "../componenets/User/courseDetailBannerSix/index";
 import Footer from "../componenets/User/footer/Index";
 import { userApi } from "../constants/axios";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Spin } from "antd";
 
 function CourseDetails() {
   const [courseData, setCourseData] = useState({});
   const [courseImages, setCourseImages] = useState([]);
+  const [loading, setLoading] = useState(false);
   const params = useParams();
   useEffect(() => {
     console.log("hoo");
-    
+    setLoading(true);
     getData(Number(params.id));
   }, []);
 
@@ -25,16 +27,18 @@ function CourseDetails() {
 
   const getData = async (id: number) => {
     console.log("oooohhhaaa");
-    
+
     await userApi.post("/getOneCourse", { id: id }).then((response) => {
       setCourseData(response.data.data.data);
-      setCourseImages(response.data.data.images)
-      console.log("res",response.data.data);
+      setCourseImages(response.data.data.images);
+      setLoading(false);
+      console.log("res", response.data.data);
     });
   };
 
   return (
     <>
+      <Spin spinning={loading} >
       <OfferBanner />
       <AppBAr />
       <CourseBannerOne data={courseData} />
@@ -44,6 +48,7 @@ function CourseDetails() {
       <CourseBannerFive data={courseData} />
       <CourseBannerSix images={courseImages} />
       <Footer />
+      </Spin>
     </>
   );
 }
