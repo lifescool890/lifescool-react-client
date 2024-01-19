6d84177ab8d0450525bc8b2d2f93cbc20fcac7d8
unknown <amanahammedkt@gmail.com>
2024-01-19 16:13:59 +0530

    location segment

--- src/componenets/User/allCourseBannerTwo/index.tsx
+++ src/componenets/User/allCourseBannerTwo/index.tsx
@@ -1,4 +1,4 @@
-import { Row, Typography, Col } from "antd";
+import { Row, Typography, Col, Segmented } from "antd";
 import "./style.scss";
 import { userApi } from "../../../constants/axios";
 import { useEffect, useState } from "react";
@@ -8,22 +8,28 @@ import locationGrey from "../../../assets/images/icons8-location-50-grey.png"
 function index() {
   const navigate = useNavigate();
   const [data, setData] = useState([]);
-
+  const [cityPointer, setCityPointer] = useState(true)
+  const [reload, setReload] = useState(false);
   useEffect(() => {
     getData();
-  }, []);
+  }, [cityPointer]);
 
   const getData = () => {
     userApi.get("/getAllCourses").then((response) => {
-      const data =response.data.data
-      const result=data.filter((item:any)=>{
-        return item.Disable==false
+      const data = response.data.data
+      const location = cityPointer == true ? "Kozhikode" : "Kochi"
+
+      const result = data.filter((item: any) => {
+        return item.Disable == false && item.location == location
       })
-      console.log(result);
-      
+
       setData(result);
+      setReload(!reload)
     });
   };
+  const changeCity = () => {
+    setCityPointer(!cityPointer)
+  }
 
   const goTo = (id: number) => {
     navigate(`/course-details/${id}`);
@@ -32,10 +38,14 @@ function index() {
   return (
     <Row className="ac-b2-outer">
       <Typography.Title className="ac-b2-title">Courses</Typography.Title>
+      <Row className="ac-b2-segment-row">
+
+        <Segmented className="ac-b2-segment" onChange={changeCity} options={[{ label: 'Kozhikode', value: "Kozhikode" }, { label: 'Kochi', value: 'Kochi' }]} />
+      </Row>
       {data?.map((item: any) => {
         return (
           <>
-            <Row className="ac-b2-row" onClick={() => goTo(item.id)}>
+            <Row className="ac-b2-row" onClick={() => goTo(item.id)} key={item.id}>
               <Col className="ac-b2-img-col" md={5}>
                 <img
                   className="ac-b2-img"
@@ -47,14 +57,14 @@ function index() {
                 <Typography.Title
                   level={4}
                   className="ac-b2-desc-title heading"
-                  style={{marginBottom:"2.5px"}}
+                  style={{ marginBottom: "2.5px" }}
                 >
-                {item.courseName}
+                  {item.courseName}
                 </Typography.Title>
                 <Typography>
                   <Row>
-                <img className="location-grey" src={locationGrey} alt="" />
-                  <p className="ac-b2-location" style={{marginBottom:"2.5px"}}>{item.location}</p>
+                    <img className="location-grey" src={locationGrey} alt="" />
+                    <p className="ac-b2-location" style={{ marginBottom: "2.5px" }}>{item.location}</p>
                   </Row>
                 </Typography>
                 <Typography className="ac-b2-desc raleway">
